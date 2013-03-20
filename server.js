5cc3d6538d1221aa05c6be66a0bce7a2409408d2
Tanzeeb Khalili <tanzeeb@xtremelabs.com>
2013-03-19 20:59:19 -0400

    initial server

---
+++ server.js
@@ -0,0 +1,8 @@
+var net = require('net');
+
+var inbound = net.createServer(function() {});
+var outbound = net.createServer(function() {});
+
+server.listen(1337, function() { });
+server.listen(6969, function() { });
+
